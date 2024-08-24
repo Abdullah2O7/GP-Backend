c909462d345403b42f3aa20a5a0709db2ad675d8
Abdullah <abdullah.mohamed1047@gmail.com>
2024-08-24 12:52:50 +0300

    fixxxx

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = '3da675e0576d436eb0a695855d1a7430'
     MONGO_URI = 'mongodb+srv://abdullahmohamed1047:HuexVTFWCHGP8wXU@cluster0.ydoaic1.mongodb.net/'
     SENDER_EMAIL = os.getenv('SENDER_EMAIL')
     SENDER_PASSWORD = os.getenv('SENDER_PASSWORD')
