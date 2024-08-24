9a8920f0c0b6ef34c550d45aacf8da739e9c32da
Abdullah <abdullah.mohamed1047@gmail.com>
2024-08-24 13:44:13 +0300

    no security

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 class Config:
     SECRET_KEY = '3da675e0576d436eb0a695855d1a7430'
     MONGO_URI = 'mongodb+srv://abdullahmohamed1047:HuexVTFWCHGP8wXU@cluster0.ydoaic1.mongodb.net/'
-    SENDER_EMAIL = os.getenv('SENDER_EMAIL')
-    SENDER_PASSWORD = os.getenv('SENDER_PASSWORD')
+    SENDER_EMAIL = 'abdullah.mohamed1047@gmail.com'
+    SENDER_PASSWORD = 'ozwd vhmw aada pxnq'
