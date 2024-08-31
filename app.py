1590e99cb4cf8c3afee98fc27f38ee567f8e58c2
Abdullah <abdullah.mohamed1047@gmail.com>
2024-08-31 17:33:45 +0300

    fix errors

--- app.py
+++ app.py
@@ -593,7 +593,7 @@ def delete_all_tests(current_user):
 # --------------- gemini -------------------------
 
 # Configure Google Generative AI API
-genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+genai.configure(api_key=app.config['GOOGLE_API_KEY'])
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 @app.route('/api/ask-gemini', methods=['POST'])
--- config.py
+++ config.py
@@ -6,5 +6,6 @@ class Config:
     MONGO_URI = 'mongodb+srv://abdullahmohamed1047:HuexVTFWCHGP8wXU@cluster0.ydoaic1.mongodb.net/'
     SENDER_EMAIL = 'abdullah.mohamed1047@gmail.com'
     SENDER_PASSWORD = 'ozwd vhmw aada pxnq'
+    GOOGLE_API_KEY="AIzaSyDOjLjuJzlAEfN7VysS80tdPsGpsNiVZOI"
 
 # mongodb://localhost:27017
