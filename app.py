c72267f5fc648bd217bc18e15f28bd791b9186aa
Abdullah Mohamed <101477947+Abdullah2O7@users.noreply.github.com>
2025-01-21 18:28:31 +0200

    Update port

--- app.py
+++ app.py
@@ -1182,4 +1182,5 @@ def get_journals(current_user):
 # ------------------------------------- The End :) ------------------------
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Azure-provided port
+    app.run(host="0.0.0.0", port=port)
