12c3c4b2ef9ce5c03585fc5be6836dfbbd87ee50
Abdullah <abdullah.mohamed1047@gmail.com>
2025-01-25 15:32:09 +0200

    updata the key

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config:
     MONGO_URI = 'mongodb+srv://abdullahmohamed1047:HuexVTFWCHGP8wXU@cluster0.ydoaic1.mongodb.net/'
     SENDER_EMAIL = 'abdullah.mohamed1047@gmail.com'
     SENDER_PASSWORD = 'ozwd vhmw aada pxnq'
-    GOOGLE_API_KEY="AIzaSyDF4-cxufrDF0ixCCRoGcXQnIgmk1HCkPQ"
+    GOOGLE_API_KEY="AIzaSyBHP5VbKPIULnM-ZpyM8mL3XIvJd_n5OZ4"
+    DEEPSEEK_API_KEY= "sk-6fd2c14211d849b1a86a591ceb5b6fa4"
 
 # mongodb://localhost:27017
 # AIzaSyATVShEBpb9rGD8wDQtkagzzL6nph6N398
