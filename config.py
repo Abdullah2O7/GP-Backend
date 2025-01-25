e7e7ba0ea2032a54a805626e5b6360ba0db417e8
Abdullah <abdullah.mohamed1047@gmail.com>
2025-01-25 15:40:00 +0200

    update key

--- config.py
+++ config.py
@@ -10,6 +10,5 @@ class Config:
     DEEPSEEK_API_KEY= "sk-6fd2c14211d849b1a86a591ceb5b6fa4"
 
 # mongodb://localhost:27017
-
 # AIzaSyATVShEBpb9rGD8wDQtkagzzL6nph6N398
 # AIzaSyDF4-cxufrDF0ixCCRoGcXQnIgmk1HCkPQ
